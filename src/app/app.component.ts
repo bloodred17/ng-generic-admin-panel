20c804b627d498a52c42d327763c183f218ae8cb
Ankur Dutta <ankur.611@gmail.com>
2020-04-04 20:50:41 +0530

    mat toolbar and drawer working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'material-test';
+  widthSideBarExpanded = 250;
+  widthSideBarCollapsed = 50;
+  widthSideBar = this.widthSideBarExpanded;
+  isOpen = true;
+
+  toggleSideBar() {
+    if (this.widthSideBar === this.widthSideBarExpanded) {
+      this.widthSideBar = this.widthSideBarCollapsed;
+      this.isOpen = false;
+    } else {
+      this.widthSideBar = this.widthSideBarExpanded;
+      this.isOpen = true;
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { DemoMaterialModule } from './material-module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import 'hammerjs';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -10,9 +13,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    DemoMaterialModule,
+    FormsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
 })
 export class AppModule { }
