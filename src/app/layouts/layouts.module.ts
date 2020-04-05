07bb6ba51f5522123abe7d8811c3613a8f4381b6
Ankur Dutta <ankur.611@gmail.com>
2020-04-05 11:27:11 +0530

    toolbar

--- src/app/layouts/layouts.module.ts
+++ src/app/layouts/layouts.module.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { AdminPanelComponent } from './admin-panel/admin-panel.component';
 import {DemoMaterialModule} from '../material-module';
 import { NavigationComponent } from './navigation/navigation.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
 
 
 
 @NgModule({
-    declarations: [AdminPanelComponent, NavigationComponent],
+    declarations: [AdminPanelComponent, NavigationComponent, ToolbarComponent],
     exports: [
         AdminPanelComponent
     ],
